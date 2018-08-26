8db507ca5fb398bfa6e75df0d4d930945b194bda
Pat Cavit <github@patcavit.com>
2018-08-25 22:57:09 -0700

    refactor: modern jsisms for modular-css-paths

--- packages/paths/paths.js
+++ packages/paths/paths.js
@@ -1,15 +1,15 @@
 "use strict";
 
-var from = require("resolve-from");
+const from = require("resolve-from");
 
-module.exports = function(args) {
-    var options = Object.assign(
-            Object.create(null),
-            {
-                paths : [],
-            },
-            args
-        );
+module.exports = (args = {}) => {
+    const options = Object.assign(
+        Object.create(null),
+        {
+            paths : [],
+        },
+        args
+    );
     
     return (src, file) => {
         var result;
