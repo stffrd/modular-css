4401e846fe74ea32e038d5111144faa550b77857
Pat Cavit <github@patcavit.com>
2018-09-06 23:47:55 -0700

    test: allow property matchers for matchFileSnapshot

--- packages/test-utils/expect-file-snapshot.js
+++ packages/test-utils/expect-file-snapshot.js
@@ -5,10 +5,11 @@ const fs = require("fs");
 const { toMatchSnapshot } = require("jest-snapshot");
 
 expect.extend({
-    toMatchFileSnapshot(source) {
+    toMatchFileSnapshot(source, ...args) {
         return toMatchSnapshot.call(
             this,
             fs.readFileSync(source, "utf8"),
+            ...args,
         );
     },
 });
